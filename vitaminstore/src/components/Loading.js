9dd090a6ac02e5035b16ab48fb6e49e49d4ecbb6
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-03-15 19:44:31 -0500

    Final UX design touches

--- vitaminstore/src/components/Loading.js
+++ vitaminstore/src/components/Loading.js
@@ -2,10 +2,15 @@ import React from "react";
 import './Loading.css';
 
 const Loading = () => (
-    <div className="spinner">
-        <div className="double-bounce1"></div>
-        <div className="double-bounce2"></div>
+    <div>
+        <div className="spinner">
+            <div className="double-bounce1"></div>
+            <div className="double-bounce2"></div>
+        </div>
+
+        <h1>Once these circles disappear begin scrolling!</h1>
     </div>
+
 );
 
 export default Loading;
